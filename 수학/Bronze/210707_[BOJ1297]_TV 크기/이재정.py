96d253d166b01fd38d9c914fd00876f10bfc0ec0
aboova <wowjd1375@naver.com>
2021-07-07 22:11:36 +0900

    Bronze_1297

--- 수학/Bronze/210707_[BOJ1297]_TV 크기/이재정.py
+++ 수학/Bronze/210707_[BOJ1297]_TV 크기/이재정.py
@@ -1,1 +1,8 @@
-day-2
+diag, height, width = map(int, input().split())
+
+sum_square = (height**2) + (width**2)
+
+width = ((diag**2)*(width**2))/sum_square
+height = ((diag**2)*(height**2))/sum_square
+
+print(int(height**0.5), int(width**0.5))
