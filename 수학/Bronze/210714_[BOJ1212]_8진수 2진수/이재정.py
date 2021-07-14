78c0d6b2b8223b0a071c9917a7d358bd99c65b28
aboova <wowjd1375@naver.com>
2021-07-14 15:33:59 +0900

    백준 열받는다...

---
+++ 수학/Bronze/210714_[BOJ1212]_8진수 2진수/이재정.py
@@ -0,0 +1,18 @@
+n = bin(int(input(), 8))[2:]
+print(n)
+
+# 알고리즘으로 풀려고 했는데 자꾸 시간 초과..
+# a = input()
+# arr = []
+
+# for i in a:
+#     arr.append(int(i))
+
+# b = arr[0]
+
+# for i in range(1, len(arr)):
+#     b *= 8
+#     b += arr[i]
+# print(bin(b)[2:])
+
+
