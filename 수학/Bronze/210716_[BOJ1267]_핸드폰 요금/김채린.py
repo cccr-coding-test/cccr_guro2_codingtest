59c3849baf87ca00a8cdad60d1cfd25c7cfc803e
zzerii <yuciee@naver.com>
2021-07-17 01:34:35 +0900

    chaelin code

---
+++ 수학/Bronze/210716_[BOJ1267]_핸드폰 요금/김채린.py
@@ -0,0 +1,16 @@
+n=int(input())
+li=list(map(int,input().split()))
+
+y = 0
+m = 0
+
+for i in range(n):
+    y += ((li[i] // 30) + 1) * 10
+    m += ((li[i] // 60) + 1) * 15
+    
+if y < m:
+    print("Y", y)
+elif m < y:
+    print("M", m)
+else:
+    print("Y", "M", y)
