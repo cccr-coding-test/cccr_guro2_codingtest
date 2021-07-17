0c73f0749b2a3198c7c4846cf84010e1a7cb5292
zzerii <yuciee@naver.com>
2021-07-18 01:52:42 +0900

    chaelin code

---
+++ 수학/Bronze/210717_[BOJ1284]_집 주소/김채린.py
@@ -0,0 +1,18 @@
+while 1:
+    
+    n = input()
+    
+    if n == '0':
+        break
+        
+    result = len(n)+1
+    
+    for i in n:
+        if i == '0':
+            result += 4 
+        elif i == '1':
+            result += 2
+        else:
+            result += 3
+            
+    print(result)
