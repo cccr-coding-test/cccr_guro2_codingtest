b5c394400c4ba430744047cf702b16f2c12ca8e5
Jaden Park <57394605+parkbyungnam@users.noreply.github.com>
2021-07-21 01:22:03 +0900

    Update 이코테_1이될때까지_박병남.py

--- 그리디/이코테_1이될때까지_박병남.py
+++ 그리디/이코테_1이될때까지_박병남.py
@@ -1,9 +1,11 @@
-n,k=map(int,input().split())
-count = 0
-while n>1:
-    count += n%k
-    temp = n//k
-    if temp:
-        n = temp
-        count+=1
-print(count)
+n,k = map(int,input().split())
+temp = (n//k) * k
+count = n - temp
+while True:
+    if temp < k:
+        break
+    count += 1
+    temp //= k
+
+result = count + temp - 1
+print(result)
