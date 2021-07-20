4ee5fa219e81bacb49ad06bc71ca2ab45bbed53d
Jaden Park <57394605+parkbyungnam@users.noreply.github.com>
2021-07-21 01:29:14 +0900

    Update 이코테_1이될때까지_박병남.py

--- 그리디/이코테_1이될때까지_박병남.py
+++ 그리디/이코테_1이될때까지_박병남.py
@@ -1,3 +1,16 @@
+### solution 1
+n,k=map(int,input().split())
+count = 0
+while n>1:
+    count += n%k
+    temp = n//k
+    if temp:
+        n = temp
+        count+=1
+print(count)
+
+
+### solution 2
 n,k = map(int,input().split())
 temp = (n//k) * k
 count = n - temp
