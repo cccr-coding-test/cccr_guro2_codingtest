4250c728120bcfbb7f0578cd909e1c9799267aa9
unknown <JaeJung>
2021-07-25 03:49:49 +0900

    어렵다

---
+++ DFS_BFS/미로탈출/이재정.py
@@ -0,0 +1,36 @@
+n, m = map(int, input().split())
+matrix = [[0]]*n
+
+for i in range(n):
+  matrix[i] = list(map(int, input()))
+
+direct = [
+  [1, 0],
+  [0, 1],
+  [1, 1]
+]
+
+cnt = 1
+
+def bfs(y, x, cnt):
+
+  if y == (n-1) and x == (m-1):
+    print(cnt)
+
+  for i in range(3):
+    ny = y + direct[i][0]
+    nx = x + direct[i][1]
+    if ny < 0 or nx < 0 or ny >= n or nx >= m:
+      continue
+    
+    if matrix[ny][nx] ==0:
+      continue
+    if matrix[ny][nx] == 1:
+      cnt += 1
+      bfs(ny, nx, cnt)
+
+
+print(bfs(0, 0, cnt))
+
+
+
