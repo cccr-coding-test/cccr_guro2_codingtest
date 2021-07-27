3f294abe053473eaf70e7caaadf6b5ec3333ddf4
ndiepray <86943561+ndiepray@users.noreply.github.com>
2021-07-27 19:46:50 +0900

    Update 이승환.py

--- 구현/왕실의 나이트/이승환.py
+++ 구현/왕실의 나이트/이승환.py
@@ -1,18 +1,20 @@
+# 현재 나이트의 위치 입력
 location = input()
-side = int(location[1])
-updown = int(location[0])
+row = int(location[1])
+column = int(ord(location[0])) - int(ord('a')) + 1
 
 count= 0
 
-#나이트가 갈수 있는 모든 방향
+#나이트가 이동할 수 있는 8가지 방향 정의
 routes = [(1,2), (1,-2), (2,1), (2,-1) ,(-1,2), (-1,-2), (-2,1), (-2,2)]
 
+# 8가지 방향에 대한 위치로 이동 가능한지 확인
 for route in routes:
-    #나이트의 초기 위치에서 다음 위치 지정
-    next_side = side + route[1]
-    next_updown = updown + route[0]
-    #나이트의 위치가 정사각형 안에 존재할 때 카운트 증가
-    if next_side >= 1 or next_side <= 8 or next_updown >=1 or next_updown <=8:
+    # 나이트의 초기 위치에서 다음 위치 지정
+    next_row = row + route[0]
+    next_column = column + route[1]
+    # 나이트의 위치가 8 X 8 안에 존재할 때 카운트 증가
+    if next_row >= 1 and next_row <= 8 and next_column >=1 and next_column <=8:
         count += 1
 
 print(count)
