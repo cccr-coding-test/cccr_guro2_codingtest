5edcbac2f3af680ddd4429cfbf1f79500f23da7b
BaekSeongTak <86943528+BaekSeongTak@users.noreply.github.com>
2021-08-01 05:34:33 +0900

    tak's code

--- 그리디/이코테_큰수의법칙_박병남.py
+++ 그리디/이코테_큰수의법칙_박병남.py
@@ -1,8 +1,7 @@
-# n,m,k=map(int,input().split())
-# mylist=list(map(int,input().split()))
-# mylist.sort(reverse=True)
-# a=m//(k+1)
-# print(mylist[0]*(m-a)+mylist[1]*a)
+n,m,k=map(int,input().split())
+mylist=list(map(int,input().split()))
+mylist.sort(reverse=True)
+a=m//(k+1)
+print(mylist[0]*(m-a)+mylist[1]*a)
 
 
-print(8//3)
