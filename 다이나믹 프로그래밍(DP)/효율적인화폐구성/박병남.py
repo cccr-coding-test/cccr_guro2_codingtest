06360721114b45ab71a91ba959c58166ad377ca8
Jaden Park <57394605+parkbyungnam@users.noreply.github.com>
2021-08-01 21:07:44 +0900

    Update 박병남.py

--- 다이나믹 프로그래밍(DP)/효율적인화폐구성/박병남.py
+++ 다이나믹 프로그래밍(DP)/효율적인화폐구성/박병남.py
@@ -1,15 +1,15 @@
 n,m=map(int,input().split())
 coins=[int(input()) for _ in range(n)]
-d=[-1]*(10001)
+canMake=[False]*(10001)
 
 for coin in coins:
-    d[coin]=1
+    canMake[coin]=1
 
 for i in range(1,m+1):
-    if d[i]!=-1:
+    if canMake[i]:
         for coin in coins:
-            if d[coin+i]==-1:
-                d[coin+i]=d[i]+1
+            if canMake[coin+i] is False:
+                canMake[coin+i]=canMake[i]+1
             else:
-                d[i+j]=min(d[i+j],d[i]+1)
-print(d[m])
+                canMake[i+j]=min(canMake[i+j],canMake[i]+1)
+print(canMake[m])
