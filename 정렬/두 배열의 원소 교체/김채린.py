adfd6608e1f82ecb0c239d2af8bf6054f6f39080
zzerii <yuciee@naver.com>
2021-08-02 21:04:03 +0900

    chaelin code

--- 정렬/두 배열의 원소 교체/김채린.py
+++ 정렬/두 배열의 원소 교체/김채린.py
@@ -2,9 +2,18 @@ n, k = map(int, input().split())
 
 a = list(map(int, input().split()))
 b = list(map(int, input().split()))
+# n,k=5,3
+# a=[1,2,5,4,3]
+# b=[5,5,6,6,5]
 a.sort()
 b.sort(reverse=True)
+print(a, b)
 for i in range(k):
-    a[i], b[i] = b[i], a[i]
+    if a[i]>=b[i]:
+        break
+    else:
+        a[i], b[i] = b[i], a[i]
+        print("a=",a)
+
 
 print(sum(a))
