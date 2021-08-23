a49eb5691ddd38967ef1bab19a994a83e8a2be19
unknown <JaeJung>
2021-08-23 19:01:40 +0900

    o

---
+++ 다이나믹 프로그래밍(DP)/이코테_금광/이재정.py
@@ -0,0 +1,26 @@
+T = int(input()) 
+
+for _ in range(T): 
+    N, M = map(int, input().split()) 
+    tmp = list(map(int, input().split())) 
+    gold = [] 
+    idx = 0 
+    
+    for _ in range(N): 
+        gold.append(tmp[idx:idx+M]) 
+        idx += M 
+    
+    for j in range(1, M): 
+        for i in range(N): 
+            if i == 0: 
+                gold[i][j] = max(gold[i][j-1] + gold[i][j], gold[i+1][j-1] + gold[i][j]) 
+            elif i == N-1: gold[i][j] = max(gold[i-1][j-1] + gold[i][j], gold[i][j-1] + gold[i][j]) 
+            else: gold[i][j] = max(gold[i-1][j-1] + gold[i][j], gold[i][j-1] + gold[i][j], gold[i+1][j-1] + gold[i][j]) 
+            
+    result = 0 
+    
+    for g in gold: 
+        result = max(result, max(g)) 
+
+print(result)
+
