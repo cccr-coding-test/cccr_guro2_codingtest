1df872aab5fdb138010e1930c4629cbb58ca958d
Jaden Park <57394605+parkbyungnam@users.noreply.github.com>
2021-08-26 01:23:10 +0900

    Update 박병남.py

--- 그리디/이코테_문자열뒤집기/박병남.py
+++ 그리디/이코테_문자열뒤집기/박병남.py
@@ -1,1 +1,20 @@
 
+ss=input()
+
+old_s=ss[0]
+block_0,block_1=0,0
+
+
+if old_s=='0':
+    block_0+=1
+else:
+    block_1+=1
+for s in ss:
+    if old_s!=s:
+        if s=='1':
+            block_1+=1
+        else:
+            block_0+=1
+        old_s=s
+
+print(min(block_0,block_1))
