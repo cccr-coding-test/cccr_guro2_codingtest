1a0f95e7b3560f813cd283298913d24c91aa26ba
nxxxtyetdecided <86827063+nxxxtyetdecided@users.noreply.github.com>
2021-09-13 20:04:30 +0900

    Create 정미정.py

---
+++ 정렬/이코테_실패율/정미정.py
@@ -0,0 +1,21 @@
+def solution(N, stages):
+    user = len(stages)
+    result = []
+    ans = []
+    for i in range(1, N+1):
+        people = stages.count(i)
+        if people == 0:
+            result.append((0, i))
+            continue
+        fail = people / user
+        result.append((fail, i))
+        user -= people
+    result.sort(key = lambda x: (-x[0], x[1]))
+    for i in range(N):
+        a, b = result[i]
+        ans.append(b)
+    return ans
+ 
+N = 5
+stages = [1, 2, 2, 1, 3]
+print(solution(N, stages))
