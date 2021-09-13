a833fc6f50ae836c31afa92c21b338df99bae755
nxxxtyetdecided <86827063+nxxxtyetdecided@users.noreply.github.com>
2021-09-13 19:55:50 +0900

    Create 정미정.py

---
+++ 그리디/이코테_문자열뒤집기/정미정.py
@@ -0,0 +1,15 @@
+s = input()
+ 
+zero, one = 0, 0
+ 
+state = s[0]
+if state == '0': zero += 1
+else: one += 1
+ 
+for i in s:
+    if i != state:
+        state = i
+        if state == '0': zero += 1
+        else: one += 1
+ 
+print(zero if zero < one else one)
